29cf8530033a676d733d30c9ad6c188b19893919
hcham <hcham@hdtel.co.kr>
2017-09-25 14:35:07 +0900

    #1 tttttttt

--- redmine-test/src/main/java/redmine_test/redmine_test/App.java
+++ redmine-test/src/main/java/redmine_test/redmine_test/App.java
@@ -1,13 +1,18 @@
+/*
+ * Copyright (c) 2017. (주)현대통신
+ *
+ * 이 프로그램은 저작권법에 따라 보호됩니다.
+ * 이 프로그램의 일부나 전부를 무단으로 복제하거나 배포하는 경우에는
+ * 저작권의 침해가 되므로 주의하시기 바랍니다.
+ */
 package redmine_test.redmine_test;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(final String[] args) {
+    System.out.println("test");
+  }
 }
