200e11ba8c8eb11484c31076115c80a33448e88f
hcham <hcham@hdtel.co.kr>
2017-09-26 17:51:48 +0900

    @refs #1 ttttttttttttt

--- redmine-test/src/main/java/redmine_test/redmine_test/App.java
+++ redmine-test/src/main/java/redmine_test/redmine_test/App.java
@@ -12,6 +12,6 @@ package redmine_test.redmine_test;
  */
 public class App {
   public static void main(final String[] args) {
-    System.out.println("refs test2111");
+    System.out.println("refs test211122");
   }
 }
